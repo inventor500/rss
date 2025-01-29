f80269fd71747a10456414e45fd3f1365e97e62f
inventor500 <isaacganoung@gmail.com>
2025-01-29 13:19:42 -0600

    Work on extracting article contents

---
+++ get-article-contents/cmd/get-article-contents/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	getter "github.com/inventor500/rss/get-article-contents"
+)
+
+const DefaultUserAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"
+
+func main() {
+	err := mainFunc()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "An error has occured: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func mainFunc() error {
+	conf, err := parseArgs()
+	if err != nil {
+		return err
+	}
+	// TODO: Maybe support reading from stdin for post-processing only?
+	feed, err := getter.GetFeed(conf)
+	if err != nil {
+		return err
+	}
+	err = getter.EnrichFeed(feed, conf)
+	if err != nil {
+		return err
+	}
+	// FIXME: This does not print the feed, it prints a JSON representation of it
+	fmt.Println(feed)
+	return nil
+}
+
+// Parse arguments
+func parseArgs() (*getter.Config, error) {
+	// TODO: Proxy support
+	var conf getter.Config
+	flag.StringVar(&conf.RemoteUrl, "url", "", "The url to fetch the feed from. Will also attempt to read from the final command line argument received.")
+	flag.StringVar(&conf.CssSelector, "selector", "article", "The CSS selector to use when extractoring articles")
+	flag.DurationVar(&conf.Timeout, "timeout", 10*time.Second, "Duration for which to wait for each individual feed.")
+	flag.StringVar(&conf.UserAgent, "user-agent", "", fmt.Sprintf("User agent. If not specified, will read from, in order, the RSS_USER_AGENT and USER_AGENT environment variables. If neither of those is specified, will use the default user agent (%s).", DefaultUserAgent))
+	flag.Parse()
+	if conf.RemoteUrl == "" {
+		if len(flag.Args()) == 1 {
+			conf.RemoteUrl = flag.Args()[0]
+		} else {
+			fmt.Fprintf(os.Stderr, "URL was not specified.\n")
+			flag.Usage()
+			os.Exit(1)
+		}
+	}
+	if conf.UserAgent == "" {
+		conf.UserAgent = os.Getenv("RSS_USER_AGENT")
+	}
+	if conf.UserAgent == "" {
+		conf.UserAgent = os.Getenv("USER_AGENT")
+	}
+	if conf.UserAgent == "" {
+		conf.UserAgent = DefaultUserAgent
+	}
+	return &conf, nil
+}
---
+++ get-article-contents/get_types.go
@@ -0,0 +1,182 @@
+package article_getter
+
+import (
+	"compress/gzip"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	feed "github.com/mmcdole/gofeed"
+)
+
+var ErrTimeoutReached = errors.New("operation timed out")
+var ErrNoMatches = errors.New("nothing matched the selector")
+
+// The config for operations
+type Config struct {
+	RemoteUrl   string
+	CssSelector string
+	UserAgent   string
+	Timeout     time.Duration
+}
+
+// Map article URLs to their contents
+type artMap map[int]*goquery.Selection
+
+type result struct {
+	Id      int // Track which item this belongs to
+	Content *goquery.Selection
+	Err     error
+}
+
+func getPage(id int, url, selector, userAgent string, ch chan *result) {
+	res := result{Id: id}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		res.Err = err
+		ch <- &res
+		return
+	}
+	client := &http.Client{}
+	addHeaders(url, userAgent, req)
+	r, err := client.Do(req)
+	if err != nil {
+		res.Err = err
+		ch <- &res
+		return
+	}
+	defer r.Body.Close()
+	if r.StatusCode != 200 {
+		res.Err = fmt.Errorf("received error code %d", r.StatusCode)
+		ch <- &res
+		return
+	}
+	// gzip was requested in the added headers
+	var reader io.ReadCloser = r.Body
+	if r.Header.Get("content-encoding") == "gzip" {
+		reader, err = gzip.NewReader(r.Body)
+		if err != nil {
+			res.Err = err
+			ch <- &res
+			return
+		}
+		defer reader.Close()
+	}
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		res.Err = err
+		ch <- &res
+		return
+	}
+	selection := doc.Find(selector)
+	if selection.Size() > 0 {
+		res.Content = selection
+		ch <- &res
+		return
+	} else {
+		res.Err = ErrNoMatches
+		ch <- &res
+		return
+	}
+}
+
+func EnrichFeed(feed *feed.Feed, conf *Config) error {
+	var fetchedMap artMap = make(artMap)
+	// How long to wait between requests
+	const sendDelay = 2 * time.Second
+	// Content should go in the <description> tag if this is RSS, or in <content> for atom
+	urls := getUrls(feed, conf.RemoteUrl)
+	urlTimeout := time.Duration(len(urls)) // Convert int to time.Duration to avoid compile errors
+	timeToWait := (urlTimeout * conf.Timeout) + (urlTimeout*sendDelay - 1)
+	ch := make(chan *result, len(urls))
+	for id, url := range urls {
+		go getPage(id, url, conf.CssSelector, conf.UserAgent, ch)
+		// Try and avoid being flagged as a bot
+		time.Sleep(sendDelay)
+	}
+	for i := 0; i < len(urls); i++ {
+		select {
+		case r := <-ch:
+			if r.Err != nil {
+				fetchedMap[r.Id] = r.Content
+			}
+			// TODO: Log the error somewhere?
+		case <-time.After(timeToWait):
+			return errors.Join(fmt.Errorf("waited %s seconds", timeToWait), ErrTimeoutReached)
+		}
+	}
+	for id, doc := range fetchedMap {
+		if doc == nil {
+			continue
+		}
+		h, err := doc.Html()
+		if err == nil {
+			feed.Items[id].Content = h
+		}
+	}
+	return nil
+}
+
+func GetFeed(conf *Config) (*feed.Feed, error) {
+	client := http.Client{}
+	req, err := http.NewRequest("GET", conf.RemoteUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	addHeaders(conf.RemoteUrl, conf.UserAgent, req)
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var reader io.ReadCloser = res.Body
+	if res.Header.Get("content-encoding") == "gzip" {
+		r, err := gzip.NewReader(res.Body)
+		if err != nil {
+			return nil, err
+		}
+		defer r.Close()
+		reader = r
+	}
+	parser := feed.NewParser()
+	return parser.Parse(reader)
+}
+
+func addHeaders(url, userAgent string, req *http.Request) {
+	req.Header.Add("Referer", url)            // Firefox can send the url of the page as its own referer
+	req.Header.Add("User-Agent", userAgent)   // Override Go's default
+	req.Header.Add("Sec-GPC", "1")            // Global Privacy Control
+	req.Header.Add("Accept-Encoding", "gzip") // Try to use compression
+	req.Header.Add("Sec-Fetch-Dest", "document")
+	req.Header.Add("Sec-Fetch-Mode", "navigate")
+	req.Header.Add("Sec-Fetch-Site", "same-origin")
+	req.Header.Add("Sec-Fetch-User", "?1")
+}
+
+func getUrls(f *feed.Feed, remote string) []string {
+	urls := make([]string, len(f.Items))
+	for c, i := range f.Items {
+		if len(i.Link) > 0 && i.Link[0] == '/' {
+			urls[c] = fixUrl(i.Link, remote)
+		} else {
+			urls[c] = i.Link
+		}
+	}
+	return urls
+}
+
+// Some feeds may specify the URL relative to the root,
+// instead of with the full protocol+domain.
+func fixUrl(u, base string) string {
+	// If URL was invalid, would have failed earlier in the program
+	loc := strings.Index(base[8:], "/")
+	if loc > 0 {
+		return base[:8+loc] + u
+	} else { // No "/" at end, and no path, e.g. "https://example.org"
+		return base + u
+	}
+}

27a396ce014fadcbbc3214437959c27b86abbe20
inventor500 <isaacganoung@gmail.com>
2025-03-04 10:49:54 -0600

    Syslog logger was causing errors
    In some environments, opening of sockets to localhost can be
    restricted. Since the syslog handler relies on this, it does not work.

--- python_feed_lib/logging.py
+++ python_feed_lib/logging.py
@@ -5,9 +5,9 @@ from logging import handlers
 from queue import SimpleQueue
 import logging
 import sys
-if sys.platform in ("aix", "darwin", "linux", "ios", "android"):
-    # Windows et al. do not support syslog
-    import syslog
+# if sys.platform in ("aix", "darwin", "linux", "ios", "android"):
+#     # Windows et al. do not support syslog
+#     import syslog
 
 def setup_logging(name: str) -> logging.Logger:
     """Set up logging for a script."""
@@ -18,11 +18,13 @@ def setup_logging(name: str) -> logging.Logger:
     logger.addHandler(async_handler)
     formatter = logging.Formatter("%(message)s")
     async_handler.setFormatter(formatter)
-    if sys.platform in ("aix", "darwin", "linux", "ios", "android"):
-        async_handlers: list[logging.Handler] = [handlers.SysLogHandler(facility=syslog.LOG_NEWS)]
-    else:
-        async_handlers = []
-    async_handlers.append(logging.StreamHandler(stream=sys.stderr))
+    # if sys.platform in ("aix", "darwin", "linux", "ios", "android"):
+    #     async_handlers: list[logging.Handler] = [handlers.SysLogHandler(facility=syslog.LOG_NEWS)]
+    # else:
+    #     async_handlers = []
+    # async_handlers.append(logging.StreamHandler(stream=sys.stderr))
+    # TODO: Syslog handler causes errors in some environments (even on UNIX)
+    async_handlers = [logging.StreamHandler(stream=sys.stderr)]
     listener = handlers.QueueListener(queue, *async_handlers)
     listener.start()
     atexit(logger.info, "Finished preparing feed")
